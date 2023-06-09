36ec38676dabc21d6d3cbe7fa82219170a55198d
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-06-10 00:45:16 +0300

    Color theme switched to tokyonight-storm

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -1,7 +1,27 @@
 ---@type ChadrcConfig 
 local M = {}
 
-M.ui = {theme = 'tokyonight'}
+M.ui = {
+  theme = 'tokyonight',
+  changed_themes = {
+    tokyonight = {
+      base_30 = {
+        black = "#24283b",
+        darker_black = "#1f2336",
+        statusline_bg = "#1f2336"
+      },
+      base_16 = {
+        base00 = "#24283b",
+        base01 = "#1f2336",
+      },
+    },
+  },
+  hl_override = {
+    CursorLine = {
+      bg = "#292e41",
+    },
+  },
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -1,3 +1,4 @@
 vim.opt.colorcolumn = "120"
+vim.opt.cursorline = true
 vim.wo.relativenumber = true
 
