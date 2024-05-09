4846f7178f16fc7398049b6fb1bee8fa33a5e6c2
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-05-09 21:41:11 +0300

    bugfix

--- modules/zsh/install_plugins.sh
+++ modules/zsh/install_plugins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Installing zsh-autosuggestions..."
-if [ ! -d $ZSH/plugins/zsh-autosuggestions ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/plugins/zsh-autosuggestions
+if [ ! -d $HOME/.oh-my-zsh/plugins/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 fi
 
 echo "Installing zsh-syntax-highlighting..."
-if [ ! -d $ZSH/plugins/zsh-syntax-highlighting ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/plugins/zsh-syntax-highlighting
+if [ ! -d $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 fi
